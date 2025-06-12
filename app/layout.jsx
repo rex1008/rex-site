37c3e3f687f5666e962893c6a46d3b806bfdc340
rex <zhangrui.coder@gmail.com>
2025-06-13 00:46:02 +0800

    animation

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import "./globals.css";
 
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetBrainsMono = JetBrains_Mono({ 
   subsets: ["latin"], 
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetBrainsMono.variable}>
         <Header />
+        <StairTransition></StairTransition>
         <PageTransition>
           {children}
         </PageTransition>
