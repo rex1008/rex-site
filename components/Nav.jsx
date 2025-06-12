eeeaa97d110851861d663715fe93c6b0e9c48c93
rex <zhangrui.coder@gmail.com>
2025-06-12 08:06:25 +0800

    nav part one

---
+++ components/Nav.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+
+const links = [
+  {
+    name: 'home',
+    path: "/"
+  },
+  {
+    name: 'home',
+    path: "/"
+  },
+  {
+    name: 'home',
+    path: "/"
+  },
+  {
+    name: 'home',
+    path: "/"
+  },
+]
+
+
+
+const Nav = () => {
+  return (
+    <nav> </nav>
+  )
+}
+
+export default Nav
