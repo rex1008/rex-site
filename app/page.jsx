00e622792e187eb51789aefaa3d83345a0244f2a
rex1008 <zhangrui.coder@gmail.com>
2025-06-14 11:02:05 +0800

    social

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Social from '@/components/Social'
+import { Button } from '@/components/ui/button'
 import { FiDownload } from 'react-icons/fi'
 
 const Home = () => {
@@ -7,12 +9,28 @@ const Home = () => {
         <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
           <div className='text-center xl:text-left'>
             <span className='text-xl'>FullStack Developer</span>
-            <h1 className='h1'>
-              Hello I&apos;m <br /><span>Rex</span>
+            <h1 className='h1 mb-6'>
+              Hello I&apos;m <br /><span className='text-accent'>Rex</span>
             </h1>
+            <p className='max-w-[500px] mb-9 text-white/80'>I am a full stack software developer.</p>
+            <div className='flex flex-col xl:flex-row items-center gap-8'>
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className='text-xl' />
+              </Button>
+              <div className='mb-8 xl:mb-0'>
+                <Social 
+                  containerStyles="flex gap-6" 
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center 
+                    items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500 " 
+                />
+              </div>
+            </div>
           </div>
           
-          <div>photo</div>
+          <div>
+            
+          </div>
         </div>
       </div>
     </section>
